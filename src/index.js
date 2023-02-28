2576cff5370c6fd704c7fa558f14de5351800fbf
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-28 19:44:08 +0800

    feat: remove hero initial load in preparation for making the menu page

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ navButtons.forEach((e) => {
   });
 });
 
-const homePage = '<div class="hero"><h1>Freshly Made. Deliciously Satisfying. Every Time.</h1><video autoplay muted loop class="hero__video"><source src="../res/hero-video.mp4" type="video/mp4"></video></div>';
+// /* page contents */
+// const homePage = '<div class="hero"><h1>Freshly Made. Deliciously Satisfying. Every Time.</h1><video autoplay muted loop class="hero__video"><source src="../res/hero-video.mp4" type="video/mp4"></video></div>';
 
-const contentContainer = document.querySelector('#content');
-contentContainer.innerHTML = homePage;
+// /* initial page load */
+// const contentContainer = document.querySelector('#content');
+// contentContainer.innerHTML = homePage;
