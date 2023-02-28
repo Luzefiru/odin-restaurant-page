b0792a4eac1398275955eb9fde3dd2b1b221f1e5
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-01 07:15:14 +0800

    feat: modularize page contents into their own script files

---
+++ src/page-home.js
@@ -0,0 +1,3 @@
+const homePage = '<div class="hero"><h1>Freshly Made. Deliciously Satisfying. Every Time.</h1><video autoplay muted loop class="hero__video"><source src="../res/hero-video.mp4" type="video/mp4"></video></div>';
+
+export default homePage;
